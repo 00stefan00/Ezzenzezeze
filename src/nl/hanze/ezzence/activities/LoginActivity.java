5ebbb63a4db383c57e358241e67017cee04e7997
EnquiringStone <johan-langes@hotmail.com>
2013-11-12 12:08:13 +0100

    My bad

---
+++ src/nl/hanze/ezzence/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package nl.hanze.ezzence.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: johan
+ * Date: 11/12/13
+ * Time: 11:46 AM
+ */
+public class LoginActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
