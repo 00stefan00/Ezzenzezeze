585c0117adf0da580a88928c6f05b7ec2db2d327
Stefan <stefan.strijker@outlook.com>
2013-11-12 11:38:13 +0100

    header etc

--- gen/nl/hanze/ezzence/R.java
+++ gen/nl/hanze/ezzence/R.java
@@ -21,21 +21,31 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int house_60x60=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Button01=0x7f080002;
+        public static final int RelativeLayout1=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080000;
+        public static final int header_button_home=0x7f080006;
+        public static final int header_text=0x7f080005;
+        public static final int include01=0x7f080003;
+        public static final int mainheader=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int header=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f050000;
+        public static final int app_name=0x7f050003;
+        public static final int header_button_home=0x7f050002;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
@@ -64,5 +74,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int ButtonTheme=0x7f060002;
+        public static final int FooterTheme=0x7f060004;
+        public static final int HeaderTheme=0x7f060003;
     }
 }
